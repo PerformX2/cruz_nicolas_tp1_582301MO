039bb7a8af1fa8371a07809a40da0e900fd75d36
nicolascruz <2363567@cmontmorency.qc.ca>
2025-09-12 15:06:03 -0400

    inputmap+background

--- projet_jeu_nico/project.godot.gd
+++ projet_jeu_nico/project.godot.gd
@@ -1,8 +1,13 @@
 extends Control
 
+func _process(float) -> void:
+	if Input.is_action_just_pressed("play_monster"):
+		_on_monster_pressed()
+	if Input.is_action_just_pressed("play_kitty"):
+		_on_kitty_pressed()
+
 func _on_monster_pressed() -> void:
 	$CanvasLayer/monster/AudioStreamPlayer.play()
-
 
 func _on_kitty_pressed() -> void:
 	$CanvasLayer/kitty/AudioStreamPlayer.play()
