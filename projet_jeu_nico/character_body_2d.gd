854c3435b2f9a4334fd3b352b21cb4c9512a7eda
Nicolas Cruz <143363153+PerformX2@users.noreply.github.com>
2025-09-12 13:57:14 -0400

    Audio fixing

--- projet_jeu_nico/character_body_2d.gd
+++ projet_jeu_nico/character_body_2d.gd
@@ -4,6 +4,6 @@ extends CharacterBody2D
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
-		anim.play("run")
+		anim.play("running")
 	else:
 		anim.play("standing")
--- projet_jeu_nico/project.godot.gd
+++ projet_jeu_nico/project.godot.gd
@@ -1,5 +1,8 @@
 extends Control
 
-
 func _on_monster_pressed() -> void:
 	$CanvasLayer/monster/AudioStreamPlayer.play()
+
+
+func _on_kitty_pressed() -> void:
+	$CanvasLayer/kitty/AudioStreamPlayer.play()
