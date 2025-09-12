3a53998d2bb6e539a7d9166c8d51bf5065b32fc1
Nicolas Cruz <143363153+PerformX2@users.noreply.github.com>
2025-09-11 21:16:52 -0400

    update 1

--- projet_jeu_nico/button_déclenchement_trigger.gd
+++ projet_jeu_nico/button_déclenchement_trigger.gd
@@ -1,1 +1,4 @@
 extends Button
+
+func _on_button_interrupteur_toggle_pressed() -> void:
+	$AudioStreamPlayer_1.play()
--- projet_jeu_nico/project.godot.gd
+++ projet_jeu_nico/project.godot.gd
@@ -1,1 +1,7 @@
-extends Control
+extends Button
+
+func _on_button_déclenchement_trigger_pressed() -> void:
+	$AudioStreamPlayer_1.play()
+
+func _on_button_interrupteur_toggle_pressed() -> void:
+	$AudioStreamPlayer_2.play()
