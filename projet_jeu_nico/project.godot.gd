147a3339769aec5d1ff9809dad0b597f165c3bc6
Nicolas Cruz <143363153+PerformX2@users.noreply.github.com>
2025-09-11 21:45:54 -0400

    Arranging stuff

--- projet_jeu_nico/project.godot.gd
+++ projet_jeu_nico/project.godot.gd
@@ -1,7 +1,26 @@
-extends Button
+extends Control
 
-func _on_button_déclenchement_trigger_pressed() -> void:
-	$AudioStreamPlayer_1.play()
+# Raccourcis vers les nœuds enfants (audio)
+@onready var monster_audio: AudioStreamPlayer = $monster/AudioStreamPlayer1
+@onready var kitty_audio: AudioStreamPlayer = $kitty/AudioStreamPlayer2
 
-func _on_button_interrupteur_toggle_pressed() -> void:
-	$AudioStreamPlayer_2.play()
+func _ready() -> void:
+	# Connecte les boutons à leurs fonctions
+	$monster.pressed.connect(_on_monster_pressed)
+	$kitty.pressed.connect(_on_kitty_pressed)
+
+# Quand on clique sur le bouton "Monster"
+func _on_monster_pressed() -> void:
+	if monster_audio.playing:
+		monster_audio.stop()
+	else:
+		monster_audio.loop = true
+		monster_audio.play()
+
+# Quand on clique sur le bouton "Kitty"
+func _on_kitty_pressed() -> void:
+	if kitty_audio.playing:
+		kitty_audio.stop()
+	else:
+		kitty_audio.loop = true
+		kitty_audio.play()
